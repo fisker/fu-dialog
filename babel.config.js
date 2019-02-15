4579c836e1967498f5df82210b6087fe67125861
fisker <lionkay@gmail.com>
2019-02-15 17:52:47 +0800

    prepare for release

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        debug: true,
+        debug: false,
         useBuiltIns: false,
         exclude: ['transform-typeof-symbol'],
         modules: false,
