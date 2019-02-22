ef0112ffb6f84b95f3d738965393a6bd5d79cbfd
fisker <lionkay@gmail.com>
2019-02-22 11:09:15 +0800

    chore(rollup): update rollup

---
+++ src/utils/one.js
@@ -0,0 +1,14 @@
+function one(fn) {
+  let called = false
+  return function(...args) {
+    if (called) {
+      return
+    }
+
+    called = true
+
+    return fn.apply(this, args)
+  }
+}
+
+export default one
